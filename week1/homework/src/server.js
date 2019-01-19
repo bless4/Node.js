098ec10eee63282672a8ebbc0b4b19da6d2f10f7
bless4 <36552429+bless4@users.noreply.github.com>
2019-01-19 01:30:44 +0100

    weekone

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const http = require('http');
 
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
@@ -9,7 +7,30 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    switch (request.url){
+      case '/state':
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify( {state: state}));
+        break;
+      case '/add':
+        state++
+         response.writeHead(200, {'Content-Type': 'application/json'});
+         response.end(JSON.stringify( {state: state}));
+         break;
+      case '/subtract':
+        state--;
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify( {state: state}));
+        break;
+      case '/reset':
+        response.writeHead(200, {'Content-Type': 'application/json'});
+        state = 10;
+        response.end(JSON.stringify( {state: state}));
+        break;
+      default:
+        response.writeHead(404, {'Content-Type': 'application/json'});
+        response.end(JSON.stringify( {error: 'Not found'}));
+    }
   });
 
   return server;
