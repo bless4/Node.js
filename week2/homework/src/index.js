847e145a4adc648f444f3afc72662bef3a37d815
bless4 <36552429+bless4@users.noreply.github.com>
2019-01-26 11:27:10 +0100

    update

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -74,7 +74,7 @@ switch (process.argv[2]){
 
 
 
-console.log(process.argv);
-console.log(process.argv[0]);
-console.log(process.argv[1]);
-console.log(process.argv[2]);
+//console.log(process.argv);
+//console.log(process.argv[0]);
+//console.log(process.argv[1]);
+//console.log(process.argv[2]);
